0cb16a0ee121b4d83c03c1310ca23be4fb03cac5
mmdzov <mzov939@gmail.com>
2021-11-22 00:42:14 +0330

    working on post comments

--- src/components/Blog/BlogPost.jsx
+++ src/components/Blog/BlogPost.jsx
@@ -1,17 +1,237 @@
 import { Container } from './BlogPost.styled';
+import { useContext, useRef, useState } from 'react';
+import Context from '../../context/Context';
+import { AiFillHeart, AiOutlineHeart, AiOutlineClose } from 'react-icons/ai';
+import { Button, Input } from 'antd';
+import { FaReply } from 'react-icons/fa';
+
+const { TextArea } = Input;
 
 const BlogPost = () => {
+  const { user } = useContext(Context);
+
+  const [blogpost, setBlogpost] = useState({
+    id: '40923420tfsdjj0023',
+    title: 'Hello world from blog',
+    description:
+      // eslint-disable-next-line max-len
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, libero voluptatibus! Odio, illum? Ad placeat vel doloribus neque. Consequuntur delectus aliquam dolorum doloribus sint alias impedit placeat reprehenderit voluptate blanditiis.',
+    date: Date.now(),
+    image:
+      'https://netseo.co.uk/wp-content/uploads/2021/06/What_is_Information_Technology-11-1536x838.jpg',
+    likes: 1223,
+    liked: false,
+    comments: [
+      {
+        from: {
+          ...user,
+        },
+        comment: 'Hello world',
+        date: Date.now(),
+        replies: [
+          {
+            from: {
+              ...user,
+            },
+            to: {
+              ...user,
+            },
+            comment: 'Hello world',
+            date: Date.now(),
+            replies: [],
+          },
+        ],
+      },
+      {
+        from: {
+          ...user,
+        },
+        comment: 'Hello world',
+        date: Date.now(),
+        replies: [
+          {
+            from: {
+              ...user,
+            },
+            to: {
+              ...user,
+            },
+            date: Date.now(),
+            comment: 'Hello world',
+            replies: [],
+          },
+        ],
+      },
+    ],
+    tags: [
+      {
+        id: 342342,
+        tag: 'psfkdofs',
+      },
+      {
+        id: 342342,
+        tag: 'psfkdofs',
+      },
+      {
+        id: 342342,
+        tag: 'psfkdofs',
+      },
+    ],
+    content: `
+    <p>Hey this editor rocks 😀</p>
+    <p />
+    <p />
+    <img
+      src="https://netseo.co.uk/wp-content/uploads/2021/06/What_is_Information_Technology-11-1536x838.jpg"
+      alt="undefined"
+      style={{ float: 'none' }}
+    />
+    <p />
+    `,
+  });
+
+  const handleLikePost = () => {
+    setBlogpost((prev) => ({
+      ...prev,
+      liked: !prev.liked,
+      likes: prev.liked ? prev.likes - 1 : prev.likes + 1,
+    }));
+  };
+
+  const [value, setValue] = useState('');
+  const [reply, setReply] = useState({});
+
+  const commentSendForm = useRef();
+  const containerRef = useRef();
+  const commentSendFormInput = useRef();
+
+  const handleChange = ({ target }) => {
+    setValue(target.value);
+  };
+
+  const handleSendMessage = () => {
+    setValue('');
+  };
+
+  const handleReplyComment = (rep) => {
+    const app = document.getElementsByClassName('App')[0];
+    app.scrollTo({
+      top: commentSendForm.current.offsetTop,
+      left: 0,
+      behavior: 'smooth',
+    });
+    setReply(rep);
+  };
+
+  const clearReply = () => {
+    setReply({});
+  };
+
   return (
-    <Container>
-      <p>Hey this editor rocks 😀</p>
-      <p />
-      <p />
-      <img
-        src="https://netseo.co.uk/wp-content/uploads/2021/06/What_is_Information_Technology-11-1536x838.jpg"
-        alt="undefined"
-        style={{ float: 'none' }}
-      />
-      <p />
+    <Container ref={containerRef}>
+      <div className="header">
+        <img src={blogpost.image} alt="" />
+        <div className="">
+          <div className="title">{blogpost.title}</div>
+          <div className="description">{blogpost.description}</div>
+        </div>
+      </div>
+      <div className="tags">
+        {blogpost.tags.map((item) => (
+          <div className="tag">#{item.tag}</div>
+        ))}
+      </div>
+
+      <div className="content" dangerouslySetInnerHTML={{ __html: blogpost.content }} />
+      <div className="like-date">
+        <div className="likes">
+          <span onClick={handleLikePost}>
+            {blogpost?.liked ? <AiFillHeart color="#bb281d" /> : <AiOutlineHeart />}
+          </span>
+          {blogpost?.likes}
+        </div>
+        <div className="date">{new Date(blogpost.date).toLocaleDateString('fa-IR')}</div>
+      </div>
+
+      <form className="comment-send" ref={commentSendForm}>
+        <div className="comment-send-title">
+          {Object.keys(reply)?.length > 0 ? `Reply to ${reply?.from?.username}` : 'Add a Comment'}
+          {Object.keys(reply)?.length > 0 ? (
+            <span onClick={clearReply}>
+              <AiOutlineClose />
+            </span>
+          ) : null}
+        </div>
+        {Object.keys(reply)?.length > 0 ? (
+          <div className="reply-to-comment">
+            <span>Comment</span>
+            {reply.comment}
+          </div>
+        ) : null}
+
+        <TextArea
+          value={value}
+          onChange={handleChange}
+          placeholder="comment..."
+          id="textarea"
+          className="scroll"
+          ref={commentSendFormInput}
+          autoSize={{ minRows: 1, maxRows: 3 }}
+        />
+        <Button type="primary" onClick={handleSendMessage}>
+          Send
+        </Button>
+      </form>
+
+      <div className="comments">
+        {blogpost.comments.map((item) => (
+          <div className="comment">
+            <div className="comment-header">
+              <div className="">
+                <img
+                  className="avatar"
+                  src={item.from.avatar}
+                  style={{ width: 40, height: 40 }}
+                  alt=""
+                />
+                <div className="username">{item.from.username}</div>
+              </div>
+              <span className="replyicon" onClick={() => handleReplyComment(item)}>
+                <FaReply />
+              </span>
+            </div>
+            <div className="comment-content">{item.comment}</div>
+            <div className="comment-footer">
+              <div className="comment-date">{new Date(item.date).toLocaleDateString('fa-IR')}</div>
+            </div>
+            {item.replies.map((reply) => (
+              <div className="comment-reply">
+                <div className="comment-header">
+                  <img
+                    className="avatar"
+                    src={reply.from.avatar}
+                    style={{ width: 40, height: 40 }}
+                    alt=""
+                  />
+                  <div className="username">
+                    {reply.from.username}{' '}
+                    <span>to {reply.to.username} 3534e rweprokw epro ekrwerw </span>
+                  </div>
+                </div>
+                <div className="comment-content">{reply.comment}</div>
+                <div className="comment-footer">
+                  <span className="replyicon" onClick={() => handleReplyComment(reply)}>
+                    <FaReply />
+                  </span>
+                  <div className="comment-date">
+                    {new Date(reply.date).toLocaleDateString('fa-IR')}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </Container>
   );
 };
--- src/components/Blog/BlogPost.styled.js
+++ src/components/Blog/BlogPost.styled.js
@@ -1,5 +1,230 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const Container = styled.div`
+  width: 95%;
+  margin: 0 auto;
+  user-select: none;
+  .header {
+    margin: 20px auto;
+    position: relative;
+    border-radius: 10px;
+    overflow: hidden;
+    margin-bottom: 10px;
+    > img {
+      width: 100%;
+      max-height: 385px;
+      object-fit: cover;
+      /* opacity: 0; */
+    }
 
-`
+    > div {
+      position: absolute;
+      bottom: 0;
+      text-align: left;
+      padding: 5px 15px;
+      background: #0000004a;
+      box-shadow: -27px -45px 81px 10px inset black;
+
+      > .title {
+        font-size: 1.5rem;
+      }
+      > .description {
+        font-size: 0.8rem;
+      }
+    }
+  }
+  @media (max-width: 770px) {
+    .header {
+      > div {
+        position: unset;
+      }
+    }
+  }
+
+  .tags {
+    display: flex;
+    /* padding: 0 25px; */
+    /* height: 30px; */
+    align-items: center;
+    flex-wrap: wrap;
+    .tag {
+      padding: 0 5px;
+      margin: 0 5px;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      color: #2196f3;
+      cursor: pointer;
+    }
+  }
+
+  .content {
+    text-align: left;
+    padding: 5px 10px;
+    img {
+      width: 100%;
+      border-radius: 5px;
+      @media (min-width: 460px) {
+        max-width: 460px;
+      }
+    }
+  }
+
+  .likes {
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+    font-size: 0.9rem;
+    padding-left: 0px;
+
+    > span {
+      display: flex;
+      align-items: center;
+    }
+
+    svg {
+      font-size: 1.5rem;
+      margin-right: 5px;
+      cursor: pointer;
+    }
+  }
+
+  .date {
+    padding-top: 3px;
+  }
+
+  .reply-to-comment {
+    font-size: 0.8rem;
+    margin-bottom: 15px;
+    padding: 0 10px;
+    color: #cfcfcf;
+    span {
+      color: white;
+      margin-right: 10px;
+    }
+  }
+
+  .comment-send {
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: space-between;
+    padding: 13px 15px;
+    margin: 20px 5px;
+    border-radius: 5px;
+    margin-right: 9px;
+    position: relative;
+    /* background: black; */
+    /* box-shadow: 0 5px 10px 2px black; */
+
+    .comment-send-title {
+      font-size: 0.8rem;
+      margin-bottom: 15px;
+      /* font-weight: bold; */
+      > span {
+        position: absolute;
+        right: 22px;
+        bottom: 13px;
+        cursor: pointer;
+        font-size: 1.1rem;
+      }
+    }
+
+    > button {
+      background: #3f51b5;
+      border: unset;
+    }
+    > textarea {
+      margin-bottom: 15px;
+    }
+  }
+
+  .like-date {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 18px;
+  }
+
+  .comments {
+    margin: 25px 0px;
+    margin-right: 5px;
+    .comment {
+      background: black;
+      padding: 10px;
+      border-radius: 5px;
+      margin-bottom: 10px;
+      .comment-date {
+        margin-bottom: 5px;
+      }
+      > .comment-footer {
+        flex-direction: row-reverse;
+      }
+
+      > .comment-header {
+        align-items: center;
+        justify-content: space-between;
+        > div {
+          display: flex;
+          align-items: center;
+        }
+      }
+    }
+
+    .comment-date {
+      text-align: right;
+      font-size: 0.8rem;
+      height: 30px;
+      align-items: center;
+      display: flex;
+      justify-content: end;
+    }
+
+    .replyicon {
+      font-size: 1rem;
+      color: #bfbfbf;
+      cursor: pointer;
+    }
+
+    .username {
+      text-align: left;
+      > span {
+        color: black;
+        margin-left: 5px;
+      }
+    }
+
+    .comment-reply {
+      margin: 0 5px;
+      background: #607d8b;
+      padding: 10px;
+      border-radius: 5px;
+    }
+
+    .comment-header {
+      display: flex;
+      align-items: center;
+
+      .avatar {
+        width: 40px;
+        height: 40px;
+        border-radius: 100%;
+        margin-right: 10px;
+      }
+    }
+
+    .comment-footer {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      /* flex-direction: row-reverse; */
+    }
+    .comment-content {
+      text-align: left;
+      padding: 0 20px;
+      min-height: 30px;
+      padding-top: 5px;
+    }
+  }
+`;
