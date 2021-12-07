0f28cbc193bd73dac59953ea7f0f6df6da0b4f48
mmdzov <mzov939@gmail.com>
2021-12-07 19:28:44 +0330

    Fixed a bug

--- src/hooks/useTokenDecode.js
+++ src/hooks/useTokenDecode.js
@@ -1,12 +1,14 @@
 import jwt from 'jsonwebtoken';
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const useTokenDecode = () => {
+  const { token } = useSelector(({ main }) => main);
   const [decoded, setDecoded] = useState({});
   useEffect(() => {
     const token = localStorage.getItem('token');
     setDecoded(jwt.decode(token));
-  }, []);
+  }, [token]);
   return decoded;
 };
 
